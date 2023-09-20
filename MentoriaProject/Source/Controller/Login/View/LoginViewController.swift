1a3defc2015cfa710a6abd7149ab6cc6e4248321
Rogerio Barreira <abacuque37@hotmail.com>
2023-09-20 12:20:01 -0300

    Correção de layout, implementação da lógica do Switch para salvar o email e implementação da lógica para registrar usuário

--- MentoriaProject/Source/Controller/Login/View/LoginViewController.swift
+++ MentoriaProject/Source/Controller/Login/View/LoginViewController.swift
@@ -17,6 +17,10 @@ class LoginViewController: UIViewController {
         view.onRegister = { [weak self] in
             self?.nextRegister()
         }
+        view.onSWTap = { [weak self] tap in
+            guard let self = self else { return }
+            self.saveEmail(_sender: tap)
+        }
         view.onError = { [weak self] title, error in
             guard let self = self else { return }
             self.showMessageError(title: title, message: error)
@@ -25,15 +29,37 @@ class LoginViewController: UIViewController {
     }()
     
     let viewModel = LoginViewModel()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Entrar"
+        self.title = "Entrada"
         self.viewLogin.setupTextField(delegate: self)
     }
     
     override func loadView() {
         self.view = viewLogin
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupNavigationItem()
+        viewFormat()
+    }
+    
+    func setupNavigationItem() {
+        navigationItem.backBarButtonItem = UIBarButtonItem(
+            title: "",
+            style: .plain,
+            target: nil,
+            action: nil
+        )
+        
+        if let navigationController = self.navigationController {
+            navigationController.navigationBar.tintColor = UIColor.white
+            let textAttributes = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 24),
+                                  NSAttributedString.Key.foregroundColor: UIColor.white]
+            navigationController.navigationBar.titleTextAttributes = textAttributes
+        }
     }
     
     func setupRequest(_ email: String, _ password: String) {
@@ -43,9 +69,35 @@ class LoginViewController: UIViewController {
             self.viewLogin.loading.stopAnimating()
             switch result {
             case .success(_):
+                if viewLogin.swPassword.isOn {
+                    self.viewModel.saveEmail = true
+                    self.viewModel.defaults.set(email, forKey: "savedEmail")
+                }
                 self.nextHome()
             case .failure(let failure):
                 self.showMessageError(title: "Erro", message: failure.localizedDescription)
+                self.viewModel.saveEmail = false
+                self.viewModel.defaults.removeObject(forKey: "savedEmail")
+            }
+        }
+    }
+    
+    func saveEmail(_sender: Bool) {
+        if viewLogin.swPassword.isOn {
+            if let email = viewLogin.textEmail.text {
+                viewModel.saveEmail = true
+                viewModel.defaults.set(email, forKey: "savedEmail")
+            }
+        } else {
+            viewModel.saveEmail = false
+            viewModel.defaults.removeObject(forKey: "savedEmail")
+        }
+    }
+    
+    func viewFormat() {
+        if viewModel.saveEmail {
+            if let savedEmail = viewModel.defaults.string(forKey: "savedEmail") {
+                viewLogin.textEmail.text = savedEmail
             }
         }
     }
@@ -85,4 +137,3 @@ extension LoginViewController: UITextFieldDelegate {
         return true
     }
 }
-
